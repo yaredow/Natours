0894efc9ebb947b7acf4223ee71b01116e6999c4
yaredow <yaredyilma11@gmail.com>
2023-11-01 17:33:56 +0300

    refactor the routes and controllers into separate folder

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const fs = require('fs');
 const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// Middlewares
+// 1) Middlewares
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.use((req, res, next) => {
   console.log('hello from the middleware');
@@ -17,90 +20,11 @@ app.use((req, res, next) => {
   next();
 });
 
-// Route handlers
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
-  });
-};
+// 2) Routes
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-const getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-
-  if (id > tours.length)
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-};
-
-const updateTour = (req, res) => {
-  if (req.params.id > tours.length)
-    return res.status(200).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>',
-    },
-  });
-};
-
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length)
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-
-  res.status(202).json({
-    status: 'success',
-    data: null,
-  });
-};
-
-const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`)
-);
-
-//  Routes
-app.route('/api/v1/tours').get(getAllTours).patch(createTour);
-app.route('/api/v1/tours').get(getTour).patch(updateTour).delete(deleteTour);
-
-//  Start server
+// 3)  Start server
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
