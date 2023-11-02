c32a190373f2c03ee2529583f3a486f50170537b
yaredow <yaredyilma11@gmail.com>
2023-11-02 14:26:08 +0300

    create a param middleware in the route controllers to check for id

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,15 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  if (req.params.id * 1 > tours.length)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
