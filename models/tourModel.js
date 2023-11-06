5cd675ad2e39f324dd46603e3c16635d86208ed2
yaredow <yaredyilma11@gmail.com>
2023-11-06 14:11:56 +0300

    write a script to import and delete all data from db

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,15 +5,53 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a max group size'],
+  },
+  difficlty: {
+    type: String,
+    reguired: [true, 'A tour must have a difficlty'],
+  },
+  ratingAverage: {
     type: Number,
     default: 4.5,
   },
+  ratinQuantity: {
+    type: Number,
+    default: 0,
+  },
+  priceDiscount: Number,
   price: {
     type: Number,
     required: [true, 'A tour must have price'],
   },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  StartDates: [Date],
 });
 
 // Model
