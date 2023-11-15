1d47223fc441da3d98100ee15796353f353624b0
yaredow <yaredyilma11@gmail.com>
2023-11-15 11:45:06 +0300

    add a duplication handler for production mode

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const appError = require('./utils/apiFeatures');
+const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) Middlewares
-if (process.env.NODE_ENV !== 'development') {
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-
-// 2) Routes
+console.log(process.env.NODE_ENV);
+// 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  next(appError(`Can't find ${req.originalUrl} on this server`, 404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 app.use(globalErrorHandler);
+
 module.exports = app;
