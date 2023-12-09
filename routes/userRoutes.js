5c8f71722b4bc0dc9b3c80575adb1ce1bf8d043a
yaredow <yaredyilma11@gmail.com>
2023-12-09 10:57:48 +0200

    add a restrict middleware in the user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,20 +16,24 @@ const {
   resetPassword,
   updatePassword,
   protect,
+  restrictTo,
 } = require('../controllers/authController');
 
 const router = express.Router();
-router.route('/me').get(protect, getMe, getUser);
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/forgotPassword', forgotPassword);
 router.patch('/resetPassword/:token', resetPassword);
 
 router.use(protect);
+router.route('/me').get(getMe, getUser);
 router.patch('/updateMyPassword', updatePassword);
 router.patch('/updateMe', updateMe);
 router.delete('/deleteMe', deleteMe);
 
+// only the admin should be allowed to do the following
+
+router.use(restrictTo('admin'));
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
