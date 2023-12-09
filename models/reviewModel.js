13e650eaf33ed3782213a113953268899d666c82
yaredow <yaredyilma11@gmail.com>
2023-12-09 16:32:32 +0200

    add an aggregate middleware to calculate the ratings avarage and quantity

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema({
   review: {
@@ -44,6 +45,32 @@ reviewSchema.pre(/^find/, function (next) {
     select: 'name photo',
   });
 
+  next();
+});
+
+reviewSchema.statics.calcAverageRating = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  console.log(stats);
+
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsAverage: stats[0].avgRating,
+    ratingsQuantity: stats[0].nRating,
+  });
+};
+
+reviewSchema.pre('save', function (next) {
+  this.constructor.calcAverageRating(this.tour);
   next();
 });
 
