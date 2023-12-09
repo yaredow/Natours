e0b9fe7b372f3e80eb61d8f80a029fa2f58da91b
yaredow <yaredyilma11@gmail.com>
2023-12-09 10:42:03 +0200

    add a protect middleware in the user route to protect sensitive routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -19,17 +19,16 @@ const {
 } = require('../controllers/authController');
 
 const router = express.Router();
+router.route('/me').get(protect, getMe, getUser);
 router.post('/signup', signup);
 router.post('/login', login);
-
-router.route('/me').get(protect, getMe, getUser);
-
 router.post('/forgotPassword', forgotPassword);
 router.patch('/resetPassword/:token', resetPassword);
-router.patch('/updateMyPassword', protect, updatePassword);
 
-router.patch('/updateMe', protect, updateMe);
-router.delete('/deleteMe', protect, deleteMe);
+router.use(protect);
+router.patch('/updateMyPassword', updatePassword);
+router.patch('/updateMe', updateMe);
+router.delete('/deleteMe', deleteMe);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
