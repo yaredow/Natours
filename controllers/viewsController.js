0c0129897d1218b985613c7476f3839e20a32a20
yaredow <yaredyilma11@gmail.com>
2023-12-13 15:31:02 +0200

    add a card template

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,9 +1,17 @@
-exports.getOverview = (req, res) => {
-  // 1
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1 Get tour data from collection
+  const tour = await Tour.find();
+
+  // 2 Build template
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tour,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
